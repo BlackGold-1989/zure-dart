45383aaa17c6eb9b7a9b32d701658f86484fb844
BlackGold-1989 <bgold1118@gmail.com>
2021-07-20 21:27:50 +0100

    finish circle mode

--- lib/pages/subs/pCircleType.dart
+++ lib/pages/subs/pCircleType.dart
@@ -90,6 +90,12 @@ class _CircleTypeScreenState extends State<CircleTypeScreen> {
         AppBar().preferredSize.height -
         MediaQuery.of(context).padding.top -
         MediaQuery.of(context).padding.bottom;
+    // if (dRealWidth > dRealHeight) {
+    //   dRealHeight = dRealHeight + (
+    //   AppBar().preferredSize.height +
+    //       MediaQuery.of(context).padding.top +
+    //       MediaQuery.of(context).padding.bottom);
+    // }
 
     var scaleX = dPanelWidth / dRealWidth;
     var scaleY = dPanelHeight / dRealHeight;
@@ -99,8 +105,12 @@ class _CircleTypeScreenState extends State<CircleTypeScreen> {
     cpWidth = dScale * dRealWidth;
     cpHeight = dScale * dRealHeight;
 
-    Offset oCenter = Offset(cpWidth / 2.0 - (cpMaxPosX + cpMinPosX) / 2.0,
-        cpHeight / 2.0 - (cpMaxPosY + cpMinPosX) / 2.0);
+    print('[Size] real: ${Offset(dRealWidth, dRealHeight)}');
+    print('[Size] calc: ${Offset(cpWidth, cpHeight)}');
+    print('[Scale] value: $dScale');
+
+    Offset oCenter = Offset((cpWidth - dPanelWidth) / 2.0 - cpMinPosX,
+        (cpHeight - dPanelHeight) / 2.0 - cpMinPosY);
 
     for (var segment in lAllItems) {
       segment.zureOffSet(-oCenter.dx, -oCenter.dy);
@@ -111,15 +121,8 @@ class _CircleTypeScreenState extends State<CircleTypeScreen> {
 
     oOriginTarget = Offset(lSegments[0].posSX, lSegments[0].posSY);
 
-    final screenCenterX = MediaQuery.of(context).size.width / 2.0;
-    final screenCenterY = (MediaQuery.of(context).size.height -
-            AppBar().preferredSize.height -
-            MediaQuery.of(context).padding.top -
-            MediaQuery.of(context).padding.bottom) /
-        2.0;
-
-    setControllerScale(
-        lSegments[0].posSX - screenCenterX, lSegments[0].posSY - screenCenterY);
+    setControllerScale(lSegments[0].posSX - dRealWidth / 2.0,
+        lSegments[0].posSY - dRealHeight / 2.0);
   }
 
   void setAnimationScale() {
@@ -166,143 +169,157 @@ class _CircleTypeScreenState extends State<CircleTypeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    final screenX = MediaQuery.of(context).size.width / 2.0;
-    final screenY = (MediaQuery.of(context).size.height -
-            AppBar().preferredSize.height -
-            MediaQuery.of(context).padding.top -
-            MediaQuery.of(context).padding.bottom) /
-        2.0;
-
-    return Stack(
-      children: [
-        SafeArea(
-          child: InteractiveViewer(
-            maxScale: dScale * 2,
-            minScale: dScale,
-            onInteractionStart: (detail) {
-              oStartDrag =
-              controller.toScene(Offset.zero);
-              print('[Offset] Start Offset: $oStartDrag');
-            },
-            onInteractionUpdate: (detail) {
-              print('[Scale] Drag Scale: ${detail.scale}');
-            },
-            onInteractionEnd: (detail) {
-              oEndedDrag =
-                  controller.toScene(Offset.zero);
-              print('[Offset] Start Offset: $oEndedDrag');
-              print('[Offset] Delta Offset: ${oStartDrag - oEndedDrag}');
-              oOriginTarget = oOriginTarget - (oStartDrag - oEndedDrag) * dScale;
-            },
-            transformationController: controller,
-            scaleEnabled: true,
-            child: Stack(
-              children: [
-                if (bShownBased)
-                  ZureMainCircleDrawWidget(
-                    lModels: lSegments,
-                    dScale: dScale,
-                  ),
-                for (var hasSubSegment in lHasSubItems)
-                  if (hasSubSegment.isExpanded)
-                    ZureSegmentCircleDrawWidget(
+    return OrientationBuilder(builder: (context, orientation) {
+      var screenX = 0.0;
+      var screenY = 0.0;
+      if (orientation == Orientation.landscape) {
+        screenX = MediaQuery.of(context).size.width / 2.0;
+        screenY = (MediaQuery.of(context).size.height -
+                AppBar().preferredSize.height -
+                MediaQuery.of(context).padding.top -
+                MediaQuery.of(context).padding.bottom) /
+            2.0;
+      } else {
+        screenX = MediaQuery.of(context).size.width / 2.0;
+        screenY = (MediaQuery.of(context).size.height -
+                AppBar().preferredSize.height -
+                MediaQuery.of(context).padding.top -
+                MediaQuery.of(context).padding.bottom) /
+            2.0;
+      }
+      return Stack(
+        children: [
+          SafeArea(
+            child: InteractiveViewer(
+              maxScale: dScale * 2,
+              minScale: dScale,
+              onInteractionStart: (detail) {
+                oStartDrag = controller.toScene(Offset.zero);
+                print('[Offset] Start Offset: $oStartDrag');
+              },
+              // onInteractionUpdate: (detail) {
+              //   print('[Scale] Drag Scale: ${detail.scale}');
+              // },
+              onInteractionEnd: (detail) {
+                oEndedDrag = controller.toScene(Offset.zero);
+                print('[Offset] Start Offset: $oEndedDrag');
+                print('[Offset] Delta Offset: ${oStartDrag - oEndedDrag}');
+                oOriginTarget =
+                    oOriginTarget - (oStartDrag - oEndedDrag) * dScale;
+              },
+              transformationController: controller,
+              scaleEnabled: true,
+              child: Stack(
+                children: [
+                  if (bShownBased)
+                    ZureMainCircleDrawWidget(
+                      lModels: lSegments,
                       dScale: dScale,
-                      seModel: hasSubSegment,
                     ),
-                for (var i = 0; i < lAllItems.length; i++)
-                  lAllItems[lAllItems.length - 1 - i].zureCircleWidget(dScale,
-                      fAction: () {
-                    setState(() {
-                      lAllItems[lAllItems.length - 1 - i].zureSetChangeExpanded(
-                          !lAllItems[lAllItems.length - 1 - i].isExpanded);
-                      if (lAllItems[lAllItems.length - 1 - i].isExpanded) {
-                        List<ZureSegmentModel> friendSegments =
-                            ZureSegmentModel.zureGetFriendSegment(
-                                lAllItems[lAllItems.length - 1 - i], lAllItems);
-                        print('[Friends] length: ${friendSegments.length}');
-                        if (friendSegments.isEmpty) {
-                          friendSegments = lSegments;
-                        }
-                        for (var item in friendSegments) {
-                          if (item.id ==
-                              lAllItems[lAllItems.length - 1 - i].id) {
-                            continue;
+                  for (var hasSubSegment in lHasSubItems)
+                    if (hasSubSegment.isExpanded)
+                      ZureSegmentCircleDrawWidget(
+                        dScale: dScale,
+                        seModel: hasSubSegment,
+                      ),
+                  for (var i = 0; i < lAllItems.length; i++)
+                    lAllItems[lAllItems.length - 1 - i].zureCircleWidget(dScale,
+                        fAction: () {
+                      setState(() {
+                        lAllItems[lAllItems.length - 1 - i]
+                            .zureSetChangeExpanded(
+                                !lAllItems[lAllItems.length - 1 - i]
+                                    .isExpanded);
+                        if (lAllItems[lAllItems.length - 1 - i].isExpanded) {
+                          List<ZureSegmentModel> friendSegments =
+                              ZureSegmentModel.zureGetFriendSegment(
+                                  lAllItems[lAllItems.length - 1 - i],
+                                  lAllItems);
+                          print('[Friends] length: ${friendSegments.length}');
+                          if (friendSegments.isEmpty) {
+                            friendSegments = lSegments;
                           }
-                          item.zureSetChangeExpanded(false);
+                          for (var item in friendSegments) {
+                            if (item.id ==
+                                lAllItems[lAllItems.length - 1 - i].id) {
+                              continue;
+                            }
+                            item.zureSetChangeExpanded(false);
+                          }
                         }
-                      }
-                      var posX = lAllItems[lAllItems.length - 1 - i].posX;
-                      if (posX + cdItemDelta < screenX) {
-                        posX = screenX;
-                      }
-                      if (posX + cdItemDelta > cpWidth - screenX) {
-                        posX = cpWidth - screenX;
-                      }
-                      var posY = lAllItems[lAllItems.length - 1 - i].posY;
-                      if (posY + cdItemDelta < screenY) {
-                        posY = screenY;
-                      }
-                      if (posY + cdItemDelta > cpHeight - screenY) {
-                        posY = cpHeight - screenY;
-                      }
-                      oTarget = Offset(posX, posY);
-                      setAnimationScale();
-                    });
-                  }),
-                if (lSegments.isNotEmpty)
-                  Positioned(
-                    left: (lSegments[0].posSX - cdItemWidth / 2.0) / dScale,
-                    top: (lSegments[0].posSY - cdItemHeight / 2.0) / dScale,
-                    child: InkWell(
-                      onTap: () {
-                        bShownBased = !bShownBased;
-                        for (var segment in lSegments) {
-                          segment.zureSetChangeVisible(bShownBased);
+                        var posX = lAllItems[lAllItems.length - 1 - i].posX;
+                        if (posX + cdItemDelta < screenX) {
+                          posX = screenX;
                         }
-                        oTarget =
-                            Offset(lSegments[0].posSX, lSegments[0].posSY);
+                        if (posX + cdItemDelta > cpWidth - screenX) {
+                          posX = cpWidth - screenX;
+                        }
+                        var posY = lAllItems[lAllItems.length - 1 - i].posY;
+                        if (posY + cdItemDelta < screenY) {
+                          posY = screenY;
+                        }
+                        if (posY + cdItemDelta > cpHeight - screenY) {
+                          posY = cpHeight - screenY;
+                        }
+                        oTarget = Offset(posX, posY);
                         setAnimationScale();
-                      },
-                      child: Container(
-                        width: cdItemWidth / dScale,
-                        height: cdItemHeight / dScale,
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          border: Border.all(color: Colors.black, width: 0.5),
-                          borderRadius: BorderRadius.all(
-                              Radius.circular(cdItemHeight / 3.0 / dScale)),
-                        ),
-                        child: Center(
-                          child: Text(
-                            'PHYSICS',
-                            style: TextStyle(
-                                fontSize: tdFontNormal - dScale,
-                                color: Colors.black),
+                      });
+                    }),
+                  if (lSegments.isNotEmpty)
+                    Positioned(
+                      left: (lSegments[0].posSX - cdItemWidth / 2.0) / dScale,
+                      top: (lSegments[0].posSY - cdItemHeight / 2.0) / dScale,
+                      child: InkWell(
+                        onTap: () {
+                          bShownBased = !bShownBased;
+                          for (var segment in lSegments) {
+                            segment.zureSetChangeVisible(bShownBased);
+                          }
+                          oTarget =
+                              Offset(lSegments[0].posSX, lSegments[0].posSY);
+                          setAnimationScale();
+                        },
+                        child: Container(
+                          width: cdItemWidth / dScale,
+                          height: cdItemHeight / dScale,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            border: Border.all(color: Colors.black, width: 0.5),
+                            borderRadius: BorderRadius.all(
+                                Radius.circular(cdItemHeight / 3.0 / dScale)),
+                          ),
+                          child: Center(
+                            child: Text(
+                              'PHYSICS',
+                              style: TextStyle(
+                                  fontSize: tdFontNormal - dScale,
+                                  color: Colors.black),
+                            ),
                           ),
                         ),
                       ),
                     ),
-                  ),
-              ],
+                ],
+              ),
             ),
           ),
-        ),
-        Positioned(
-          right: cdItemDelta / 2.0,
-          top: cdItemDelta / 2.0,
-          child: IconButton(
-            onPressed: () {
-              setState(() {
-                _asyncData();
-              });
-            },
-            icon: Icon(
-              Icons.refresh,
-              color: Colors.black,
+          Positioned(
+            right: cdItemDelta / 2.0,
+            top: cdItemDelta / 2.0,
+            child: IconButton(
+              onPressed: () {
+                setState(() {
+                  _asyncData();
+                });
+              },
+              icon: Icon(
+                Icons.refresh,
+                color: Colors.black,
+              ),
             ),
           ),
-        ),
-      ],
-    );
+        ],
+      );
+    });
   }
 }
